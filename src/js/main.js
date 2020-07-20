9a0324f0999a8d3823ce164cd1c72aa05fd32536
masopego <masopego@gmail.com>
2020-07-20 22:59:46 +0200

    Fix header text and quote and add typed library

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+const sentence1 =
+  ' <span>Camaleónica: adj.,</span> se dice de aquella persona que es capaz de adaptarse a su entorno.';
+
+const sentence2 =
+  ' <span>Crítica: adj.,</span> se dice de aquella persona que analiza pormenorizadamente algo y lo valora según sus propios criterios.';
+
+const sentence3 =
+  ' <span> Minuciosa: adj.,</span> se dice de aquella persona amante de los detalles, que vela por su cuidado.';
+
+var options = {
+  strings: [sentence1, sentence2, sentence3],
+  typeSpeed: 50,
+  shuffle: true,
+  fadeOut: true,
+  loop: true,
+  backDelay: 2000,
+};
+
+var typed = new Typed('.quote', options);
